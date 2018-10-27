7936f11f8b406e343be1cbc7a0dd738b6c1aa86e
Neal <>
2018-10-27 11:06:42 +0800

    Initial commit

---
+++ JSA4Cocoa/JSA4Cocoa.h
@@ -0,0 +1,19 @@
+//
+//  JSA4Cocoa.h
+//  JSA4Cocoa
+//
+//  Created by Neal on 2018/10/27.
+//  Copyright © 2018 JSAppSugar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSA4Cocoa.
+FOUNDATION_EXPORT double JSA4CocoaVersionNumber;
+
+//! Project version string for JSA4Cocoa.
+FOUNDATION_EXPORT const unsigned char JSA4CocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSA4Cocoa/PublicHeader.h>
+
+
---
+++ JSA4CocoaTests/JSA4CocoaTests.m
@@ -0,0 +1,31 @@
+//
+//  JSA4CocoaTests.m
+//  JSA4CocoaTests
+//
+//  Created by Neal on 2018/10/27.
+//  Copyright © 2018 JSAppSugar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSA4CocoaTests : XCTestCase
+
+@end
+
+@implementation JSA4CocoaTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+
+@end
